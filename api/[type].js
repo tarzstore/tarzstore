370c107d3872229a64733b75b5ea60449cc69735
tarzstore <tarzxiter@gmail.com>
2026-08-23 22:13:52 +0700

    Update [type].js

--- api/[type].js
+++ api/[type].js
@@ -28,7 +28,7 @@ const CONFIGS = {
   },
   fakeffduo: {
     // Endpoint ini pakai domain apii.nexadev.my.id (dobel i), beda dari yang lain.
-    base: "https://apii.nexadev.my.id/fakeffduo",
+    base: "https://apii.nexadev.my.id/fakeffduo?nickname1=Tarz&nickname2=Kuntul",
     params: ["nickname1", "nickname2"],
   },
   tiktokstalker: {
